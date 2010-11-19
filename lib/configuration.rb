415c9b3f112445ee1d2fdb87f8fbf2ae7e3dc6e8
Andrew Vos <andrew.vos@gmail.com>
2010-11-19 16:26:15 +0000

    Fixed an issue with the file paths in the config

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -1,8 +1,8 @@
 class Application < Sinatra::Base
   configure do
-    set :app_file, File.expand_path(File.dirname(__FILE__), 'application.rb')
-    set :public,   File.expand_path(File.join(File.dirname(__FILE__), '..', 'public'))
-    set :views,    File.expand_path(File.join(File.dirname(__FILE__) , '..', 'views'))
+    set :app_file, File.join(File.dirname(__FILE__), 'application.rb')
+    set :public,   File.join(File.dirname(__FILE__), '..', 'public')
+    set :views,    File.join(File.dirname(__FILE__) , '..', 'views')
   end
 
   configure :production do
